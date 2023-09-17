25d32580f645ccf782f56fd8a933b0e0fcdf28b1
James Leal Vieira <james.leal2@gmail.com>
2023-09-17 00:12:18 -0300

    CHORE: adding colors

---
+++ src/shared/styles/colors.ts
@@ -0,0 +1,17 @@
+export const Colors = {
+  primary: {
+    red: "#FF7C89",
+    black: "#000000",
+    blueDark: "#3B6C82",
+    blueLight: "#00E1E8",
+    blue: "#2B4B5D",
+    white: "#FFFFFF",
+    title: "#EFEFEF",
+  },
+  button: {
+    active: "#16E3E7",
+    contrast: "#FFFFFF",
+    waiting: "#6392A6",
+    disabled: "#9EB6C2",
+  },
+};
