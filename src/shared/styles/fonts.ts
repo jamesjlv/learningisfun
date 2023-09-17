a6e833a37a95314c5299c05298b16fa861cddbdd
James Leal Vieira <james.leal2@gmail.com>
2023-09-17 00:06:00 -0300

    CHORE: added inter font in app

---
+++ src/shared/styles/fonts.ts
@@ -0,0 +1,42 @@
+import { css } from "styled-components/native";
+
+export const Font = {
+  inter: {
+    thin: css`
+      font-family: "Inter-Thin";
+      font-weight: 100;
+    `,
+    extraLight: css`
+      font-family: "Inter-ExtraLight";
+      font-weight: 200;
+    `,
+    light: css`
+      font-family: "Inter-Light";
+      font-weight: 300;
+    `,
+    regular: css`
+      font-family: "Inter-Regular";
+      font-weight: 400;
+    `,
+    medium: css`
+      font-family: "Inter-Medium";
+      font-weight: 500;
+    `,
+    semiBold: css`
+      font-family: "Inter-SemiBold";
+      font-weight: 600;
+    `,
+    bold: css`
+      font-family: "Inter-Bold";
+      font-weight: 700;
+    `,
+    extraBold: css`
+      font-family: "Inter-ExtraBold";
+      font-weight: 800;
+    `,
+    black: css`
+      font-family: "Inter-Black";
+      font-weight: 900;
+    `,
+  },
+};
