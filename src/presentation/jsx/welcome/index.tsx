cd70b097763b2b97dadccc5ba1a14e36d3028a08
James Leal Vieira <james.leal2@gmail.com>
2023-09-17 01:08:49 -0300

    CHORE: created welcome screen

--- src/presentation/jsx/welcome/index.tsx
+++ src/presentation/jsx/welcome/index.tsx
@@ -1,7 +1,41 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Button, Wrapper, Title, ButtonWrapper, Container } from "./styles";
+import { StatusBar } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Routes } from "@/main";
 
 export const WelcomeScreen = () => {
-  return <Container></Container>;
+  const { navigate } = useNavigation();
+
+  const handleNavigate = (language: string) => {
+    try {
+      navigate(Routes.Exercises, { origin: language });
+    } catch (error) {
+      console.error("Can not navigate");
+    }
+  };
+
+  return (
+    <Wrapper source={require("@/assets/images/Welcome.jpg")}>
+      <StatusBar hidden backgroundColor="transparent" translucent />
+      <Container>
+        <Title>The most fun way to learn a language.</Title>
+        <ButtonWrapper>
+          <Button
+            text="Learn English"
+            onPress={() => handleNavigate("english")}
+            disabled={false}
+            loading={false}
+          />
+          <Button
+            text="Learn German"
+            onPress={() => handleNavigate("german")}
+            disabled={false}
+            loading={false}
+          />
+        </ButtonWrapper>
+      </Container>
+    </Wrapper>
+  );
 };
--- src/presentation/jsx/welcome/styles.tsx
+++ src/presentation/jsx/welcome/styles.tsx
@@ -1,3 +1,26 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
+import { ButtonComponent } from "@/presentation/components";
 
 export const Container = styled.View``;
+
+export const Wrapper = styled.ImageBackground.attrs({
+  resizeMode: "cover",
+})`
+  flex: 1;
+  flex-direction: column-reverse;
+  padding: ${({ theme }) => theme.moderateSize.large};
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    ${theme.text.inter.bold}
+    font-size: ${theme.moderateSize.xmLarge};
+    color: ${theme.colors.primary.white};
+  `}
+`;
+
+export const ButtonWrapper = styled.View`
+  margin-top: ${({ theme }) => theme.moderateSize.xmLarge};
+`;
+
+export const Button = styled(ButtonComponent)``;
