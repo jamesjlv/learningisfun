14e555f17d676a5cc2400d415fc526f47c67d565
James Leal Vieira <james.leal2@gmail.com>
2023-09-17 01:23:20 -0300

    CHORE: adding usecase of Sentence

---
+++ src/domain/services/sentences/remote-get-sentences.tsx
@@ -0,0 +1,15 @@
+import { ISentencesModel } from "@/domain/models";
+
+interface IGetSentencesService {
+  exec: (
+    params: GetSentencesServiceNamespace.Params,
+  ) => Promise<GetSentencesServiceNamespace.Model>;
+}
+
+export namespace GetSentencesServiceNamespace {
+  export type Params = { origin: string; translation: string };
+
+  export type Model = ISentencesModel;
+
+  export type Interface = IGetSentencesService;
+}
