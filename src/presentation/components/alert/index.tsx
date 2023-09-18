a0e4ccef6176627fea8a109f919cb141fd0f17ca
James Leal Vieira <james.leal2@gmail.com>
2023-09-17 23:32:49 -0300

    FEAT: create alert component

---
+++ src/presentation/components/alert/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ErrorSVG from "@assets/icons/alert-circle.svg";
+import { Container, ErrorMessage } from "./styles";
+import { AlertProps } from "./props";
+
+export const Alert: React.FC<AlertProps> = ({
+  message = "Message default",
+  type = "error",
+}) => {
+  return (
+    <Container testID="AlertComponent" type={type}>
+      <ErrorSVG color="#FFFFFF" />
+      <ErrorMessage testID="AlertComponent-Text">{message}</ErrorMessage>
+    </Container>
+  );
+};
