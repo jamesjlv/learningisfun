27e02f18ce0dd82d6135606dbef087f9ee1c3ccf
James Leal Vieira <james.leal2@gmail.com>
2023-09-17 22:36:12 -0300

    FEAT: added tooltip with translation

--- src/presentation/components/sentenceLine/index.tsx
+++ src/presentation/components/sentenceLine/index.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
-  Container,
   SentenceToTranslate,
   SpaceForSelectedWord,
   SentenceTranslatedWrapper,
+  SentenceIsolateWordAndTooltip,
+  SentenceWordWrapper,
+  TooltipWrapper,
+  TooltipWord,
+  WrapperWordAndTooltip,
+  Tail,
 } from "./styles";
 import { WordComponent } from "../word";
 import { SentenceLineComponentProps } from "./props";
+import { LayoutChangeEvent } from "react-native";
 
 export const SentenceLineComponent: React.FC<SentenceLineComponentProps> = ({
   sentence,
@@ -16,26 +22,96 @@ export const SentenceLineComponent: React.FC<SentenceLineComponentProps> = ({
   wordsMatch,
   wordToReplace,
 }) => {
+  const [isPressing, setIsPressing] = useState({ isActive: false, word: "" });
+
   const wordTranslated = wordsMatch?.filter(
     (item) => item.origin === wordToReplace,
   )?.[0];
-  const beforeWordSentence = sentence?.split(wordTranslated?.translation!)?.[0];
-  const afterWordSentence = sentence?.split(wordTranslated?.translation!)?.[1];
+  const beforeWordSentence = sentence
+    ?.split(wordTranslated?.translation!)?.[0]
+    .split(" ");
+  const afterWordSentence = sentence
+    ?.split(wordTranslated?.translation!)?.[1]
+    ?.split(" ");
+
+  const handleMatchStrings = (str1: string, str2: string) => {
+    return (
+      str1.toLowerCase().replace(",", "") ===
+      str2.toLowerCase().replace(",", "")
+    );
+  };
 
   return (
-    <SentenceTranslatedWrapper>
-      <SentenceToTranslate>{beforeWordSentence}</SentenceToTranslate>
-      <SpaceForSelectedWord selected={!!selectedWord} onPress={onPress}>
-        {selectedWord && (
-          <WordComponent
-            disabled
-            word={selectedWord}
-            selected={false}
-            type="secondary"
-          />
-        )}
-      </SpaceForSelectedWord>
-      <SentenceToTranslate>{afterWordSentence}</SentenceToTranslate>
-    </SentenceTranslatedWrapper>
+    <>
+      <SentenceTranslatedWrapper>
+        {beforeWordSentence
+          ?.filter((item) => item !== "")
+          ?.map((word, index) => {
+            const toolTipWord = wordsMatch?.filter((item) =>
+              handleMatchStrings(item.translation, word),
+            )?.[0]?.origin;
+            const active = isPressing.word === toolTipWord;
+            return (
+              <WrapperWordAndTooltip key={`BeforeWordSentence${word}${index}`}>
+                <TooltipWrapper active={active}>
+                  <TooltipWord active={active}>{toolTipWord}</TooltipWord>
+                  <Tail active={active} />
+                </TooltipWrapper>
+                <SentenceIsolateWordAndTooltip
+                  onPressIn={() => {
+                    setIsPressing({ isActive: true, word: toolTipWord! });
+                  }}
+                  onPressOut={() =>
+                    setIsPressing({ isActive: false, word: "" })
+                  }
+                >
+                  <SentenceWordWrapper>
+                    <SentenceToTranslate>{word}</SentenceToTranslate>
+                  </SentenceWordWrapper>
+                </SentenceIsolateWordAndTooltip>
+              </WrapperWordAndTooltip>
+            );
+          })}
+        <SpaceForSelectedWord selected={!!selectedWord} onPress={onPress}>
+          {selectedWord && (
+            <WordComponent
+              disabled
+              word={selectedWord}
+              selected={false}
+              type="secondary"
+            />
+          )}
+        </SpaceForSelectedWord>
+        {afterWordSentence
+          ?.filter((item) => item !== "")
+          ?.map((word, index) => {
+            const toolTipWord = wordsMatch?.filter((item) =>
+              handleMatchStrings(item.translation, word),
+            )?.[0]?.origin;
+            const active = isPressing.word === toolTipWord;
+
+            return (
+              <WrapperWordAndTooltip key={`AfterWordSentence${word}${index}`}>
+                <TooltipWrapper active={active}>
+                  <TooltipWord active={active}>{toolTipWord}</TooltipWord>
+                  <Tail active={active} />
+                </TooltipWrapper>
+                <SentenceIsolateWordAndTooltip
+                  onPressIn={() => {
+                    setIsPressing({ isActive: true, word: toolTipWord! });
+                  }}
+                  onPressOut={() =>
+                    setIsPressing({ isActive: false, word: "" })
+                  }
+                >
+                  <SentenceWordWrapper>
+                    <SentenceToTranslate>{word}</SentenceToTranslate>
+                  </SentenceWordWrapper>
+                </SentenceIsolateWordAndTooltip>
+              </WrapperWordAndTooltip>
+            );
+          })}
+      </SentenceTranslatedWrapper>
+    </>
   );
 };
